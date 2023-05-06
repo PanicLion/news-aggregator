dbabc81e0f16193c97196d5c6c98ee6f2447e39f
Mahendra Patel <mpatel468@hotmail.co.uk>
2023-05-06 23:34:09 +0530

    added logging capability

--- app.js
+++ app.js
@@ -2,12 +2,41 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('express').Router();
+const morgan = require('morgan');
+const winston = require('winston');
 const news = require('./routes/news');
 const preferences = require('./routes/preference');
 const { signup, signin } = require('./controller/authController');
 
+const { format } = winston;
+const logger = winston.createLogger({
+    format: format.combine(
+        format.colorize(),
+        format.timestamp(),
+        format.printf((msg) => {
+            return `${msg.timestamp} [${msg.level}] ${msg.message}`;
+        })
+    ),
+    transports: [
+        // new winston.transports.File({ filename: 'combined.log', level: 'http' }) // to write the logs into a file we have to remove colorize()
+        new winston.transports.Console({ level: 'http'})
+    ],
+});
+
+
+const morganMiddleware = morgan(
+    ':method :url :status :res[content-length] - :response-time ms',
+    {
+        stream: {
+            write: (message) => logger.http(message.trim()),
+        },
+    }
+);
+
+
 const app = express();
 app.use(cors());
+app.use(morganMiddleware);
 app.use(routes);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
